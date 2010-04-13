b303019b2ae5ada638addea0a9fc30183ee22ff8
Steve Whittaker <swhitt@gmail.com>
2010-04-14 00:06:58 +0200

    Added support for special filters, allowing the user to bypass the standard handling. Added exceptions when encountering unexpected inputs.

--- lib/extjs_filterable.rb
+++ lib/extjs_filterable.rb
@@ -32,10 +32,17 @@ module ExtjsFilterable
     # * +per_page+: Sets the default per_page used by will_paginate and the plugin. If will_paginate is already set
     #   to use a certain number, uses that. If nothing is specified, defaults to 100.
     # * +include+: An array that contains the associations to load.
+    # * +special_filters+: A hash that allows custom handling to be used in the filter block. Keys are the dataIndex,
+    #   values are either a Proc object or a Symbol that points to a class method. The parameters passed to the proc
+    #   or method are the conditions array, the values array, the type of filter, and the value of the filter. 
     def extjs_filterable(merge_opts=nil)
       old_per_page = class_variable_get(:@@per_page) rescue nil
-      opts = { :per_page => (old_per_page || 100), :columns => {}, :include => []}
-      opts.merge!(merge_opts) if merge_opts
+      opts = { :per_page => (old_per_page || 100), :columns => {}, :include => [], :special_filters => {}}
+      
+      if merge_opts
+        raise ArgumentError, 'parameter hash expected' unless merge_opts.respond_to? :symbolize_keys
+        opts.merge!(merge_opts.symbolize_keys)
+      end
       
       class_variable_set(:@@per_page, opts[:per_page])
       
@@ -45,13 +52,16 @@ module ExtjsFilterable
     # +filter_and_sort_options+ will return the hash of the options that are meant to be sent to will_paginate when
     # using the +paginate_by_filter+ method. 
     def filter_and_sort_options(opts={})
+      raise ArgumentError, 'parameter hash expected' unless opts.respond_to? :symbolize_keys
+      opts.symbolize_keys!
+      
       limit = opts[:limit].try(:to_i) || class_variable_get(:@@per_page) rescue 100
       page =  ((opts[:start].try(:to_i) || 0) / limit) + 1
       
       if opts[:sort].blank?
-        sort = 'created_at'
+        sort = extjs_filterable_options[:default_sort] || 'created_at'
       else
-        if extjs_filterable_options[:columns][opts[:sort]]
+        if extjs_filterable_options[:columns][opts[:sort].to_sym]
           sort = "#{extjs_filterable_options[:columns][opts[:sort]]} #{opts[:dir]}"
         else
           sort = "#{opts[:sort]} #{opts[:dir]}"
@@ -60,21 +70,33 @@ module ExtjsFilterable
       
       conditions = ["#{primary_key} is not null"]
       values = []
-      
+            
       opts[:filter].try(:each_pair) do |index,f|
         field = f[:field]
         type  = f[:data][:type]
         value = f[:data][:value]
 
-        field = extjs_filterable_options[:columns][field]   if extjs_filterable_options[:columns][field]
+        custom_handler = extjs_filterable_options[:special_filters][field.to_sym]
+
+        if custom_handler
+          if custom_handler.kind_of? Proc
+            custom_handler.call(conditions,values,type,value)
+          elsif (custom_handler.kind_of?(String) || custom_handler.kind_of?(Symbol))
+            send(custom_handler,conditions,values,type,value)
+          else
+            raise ArgumentError, "custom handler for #{field} not Proc or Symbol" 
+          end
+        else
+          field = extjs_filterable_options[:columns][field.to_sym]   if extjs_filterable_options[:columns][field.to_sym]
       
-        case type
-        when 'string'
-          conditions << "UPPER(#{connection.quote_column_name(field)}) like ?"
-          values     << "%#{value.upcase}%"
-        when 'list'
-          conditions << "#{connection.quote_column_name(field)} IN (?)"
-          values << value.split(',')
+          case type
+          when 'string'
+            conditions << "UPPER(#{connection.quote_column_name(field)}) like ?"
+            values     << "%#{value.upcase}%"
+          when 'list'
+            conditions << "#{connection.quote_column_name(field)} IN (?)"
+            values << value.split(',')
+          end
         end
       
       end
@@ -93,7 +115,7 @@ module ExtjsFilterable
     def per_page
       @@per_page rescue nil
     end
-    
+
     # Returns the options stored by the plugin.
     def extjs_filterable_options
       read_inheritable_attribute(:extjs_filterable_options)
